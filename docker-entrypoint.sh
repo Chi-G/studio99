e401bf19530ab21303153ea4a1f4ba0683940470
Chijindu Nwokeohuru <chijindu.nwokeohuru@gmail.com>
2026-06-16 17:52:26 +0100

    Add missing docker-entrypoint.sh

---
+++ docker-entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+set -e
+
+# Optimize application for production
+echo "Caching configuration..."
+php artisan config:cache
+
+echo "Caching routes..."
+php artisan route:cache
+
+echo "Caching views..."
+php artisan view:cache
+
+# Run database migrations
+echo "Running migrations..."
+php artisan migrate --force
+
+# Start the main process
+exec "$@"
